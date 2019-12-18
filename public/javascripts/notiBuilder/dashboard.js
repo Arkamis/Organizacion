604191ddb59d6bf92c58d51e2e8c6d86b14bfa70
arkamis <miguelantonio_rodriguez@ucol.mx>
2019-12-18 14:26:34 -0600

    avance user dashboard notibuilder

--- public/javascripts/notiBuilder/dashboard.js
+++ public/javascripts/notiBuilder/dashboard.js
@@ -1,4 +1,23 @@
 (function(){
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+        $('#imgNoti').attr('src', e.target.result);
+    }
+
+    function readUrl(i){
+        if(i.files && i.files[0]){
+            if($('.epigrafe').hasClass("active") == false){
+                $('.epigrafe').toggleClass('active');
+            }
+            reader.readAsDataURL(i.files[0]);
+        }
+    }
+    //llamo cuando el input:file tiene un cambio
+    $('#imgInput').change(function(){
+        readUrl(this);
+    });
     $(document).ready(function () {
         $('#sidebarCollapse').on('click', function () {
             $('#sidebar').toggleClass('active');
